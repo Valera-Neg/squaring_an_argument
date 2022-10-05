74bd5c95d33d7dc8f664c7deb96267867b6b9002
Valera-Neg <nehovora.val@gmail.com>
2022-10-05 16:28:32 -0400

    first commit

---
+++ square.js
@@ -0,0 +1,10 @@
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+const square = (n) => n ** 2;
+const square2 = (n, p) => Math.pow(n, p);
+
+
+console.log(square(3)) //> 9
+console.log(square2(3, 2)) //> 9
